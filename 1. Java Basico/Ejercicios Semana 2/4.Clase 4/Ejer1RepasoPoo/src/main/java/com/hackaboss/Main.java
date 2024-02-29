07eed3d160cc63885c40fd396ab873fef17c4ed7
beaperezm <bpml2001@hotmail.com>
2024-02-29 12:29:16 +0100

    subiendo vector

--- 1. Java Basico/Ejercicios Semana 2/4.Clase 4/Ejer1RepasoPoo/src/main/java/com/hackaboss/Main.java
+++ 1. Java Basico/Ejercicios Semana 2/4.Clase 4/Ejer1RepasoPoo/src/main/java/com/hackaboss/Main.java
@@ -2,6 +2,13 @@ package com.hackaboss;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Verdura[] verduras = new Verdura[5];
+        verduras [0] = new Verdura(1, "coliflor", "blanca", 50, true);
+        verduras [1] = new Verdura(2, "judías verdes", "verde", 80, true);
+        verduras [2] = new Verdura(3, "espinacas", "verde", 60, true);
+        verduras [3] = new Verdura(4, "lombarda", "morado", 90, true);
+        verduras [4] = new Verdura(5, "brócoli", "verde", 50, true);
+
     }
 }
